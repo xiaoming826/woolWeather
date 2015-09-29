80d7f011a703296bc87bb4b70bd1619daa175326
xiaoming <2515238312@qq.com>
2015-09-29 13:18:01 +0800

    新增数据库相关类和实体类

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # woolWeather
+by xiaoming
---
+++ src/com/woolweather/app/db/WoolWeatherOpenHelper.java
@@ -0,0 +1,50 @@
+package com.woolweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class WoolWeatherOpenHelper extends SQLiteOpenHelper {
+	/**
+	 * 创建province  省 表
+	 */
+	public static final String CREATE_PROVINCE = "create table Province ("
+			+ "id integer primary key autoincrement," + //
+			"province_name text," + //
+			"province_code text)";
+	/**
+	 * 创建 city 表
+	 */
+	public static final String CREATE_CITY = "create table City ("
+			+ "id integer primary key autoincrement," + //
+			"city_name text," + //
+			"city_code text," +
+			"province_id integer)"
+			;
+	/**
+	 * 创建  county 县表
+	 */
+	public static final String CREATE_COUNTY = "create table County ("
+			+ "id integer primary key autoincrement," + //
+			"county_name text," + //
+			"county_code text," +
+			"city_id integer)";
+
+	public WoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+}
